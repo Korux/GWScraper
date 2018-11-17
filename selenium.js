7934abe0b44db008dab8971c6e59e4a7c6d6e777
KorsK <kdong@my.chemeketa.edu>
2018-11-16 21:35:19 -0800

    scrape cutoff info on intervals

--- selenium.js
+++ selenium.js
@@ -3,24 +3,31 @@ const chrome = require('selenium-webdriver/chrome');
 const fs = require('fs');
 const By = webdriver.By;
 const Until = webdriver.until;
+const monthInfo = {
+    0 : "Jan",
+    1 : "Feb",
+    2 : "Mar",
+    3 : "Apr",
+    4 : "May",
+    5 : "Jun",
+    6 : "Jul",
+    7 : "Aug",
+    8 : "Sep",
+    9 : "Oct",
+    10 : "Nov",
+    11 : "Dec",
 
-var options = new chrome.Options();
+};
+
+const options = new chrome.Options();
+const date = new Date();
 options.addArguments("--user-data-dir=./profile");
 
-let driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .withCapabilities(webdriver.Capabilities.chrome())
-    .setChromeOptions(options)
-    .build();
+var currDriver = false;
 
-getCutoffElements().then(eles=>{
-    saveToJson(eles,function(){
-        console.log("saved to cutoffs.json");
-        driver.quit();
-    });
-});
+setInterval(getInfo,1000);
 
-function getCutoffElements(){
+function getCutoffElements(driver){
     return new Promise(function(resolve){
         driver.get("http://game.granbluefantasy.jp/#event/teamraid041/ranking").then(()=>{
             driver.wait(Until.elementsLocated(By.className('txt-total-record')),10000,"no ele").then(function(){
@@ -42,15 +49,37 @@ function saveToJson(cutOffData,callback = function(){}){
             throw err;
         }else{
             var obj = JSON.parse(data);
+            var dateStr = ""+monthInfo[date.getMonth()]+" " + date.getDate()+ ", "+date.getHours()+":"+date.getMinutes();
             var cutOffMap = {
                 "30k" : cutOffData[0],
                 "50k" : cutOffData[1],
                 "70k" : cutOffData[2],
                 "120k" : cutOffData[3]
             };
-            obj.push(cutOffMap);
-            var json = JSON.stringify(obj);
+            var cutOffInfo = {};
+            cutOffInfo[dateStr] = cutOffMap;
+            obj.push(cutOffInfo);
+            var json = JSON.stringify(obj,null,4);
             fs.writeFile('cutoffs.json',json,callback);
         }
     });
+}
+
+function getInfo(){
+    if(date.getMinutes()%10 == 0 && !currDriver){
+        currDriver = true;
+        let driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .withCapabilities(webdriver.Capabilities.chrome())
+        .setChromeOptions(options)
+        .build();
+
+        getCutoffElements(driver).then(eles=>{
+            saveToJson(eles,function(){
+                console.log("saved to cutoffs.json");
+                console.log("saved at: ",""+monthInfo[date.getMonth()]+" " + date.getDate()+ ", "+date.getHours()+":"+date.getMinutes());
+                driver.quit();
+            });
+        });
+    }
 }
