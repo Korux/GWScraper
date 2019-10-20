66db10370fbe0cf1b1d8f3084c5ea75e4d484109
KorsK <kdong@my.chemeketa.edu>
2019-10-20 13:49:42 -0700

    date no longer const, should work with any date/GW

--- selenium.js
+++ selenium.js
@@ -20,16 +20,29 @@ const monthInfo = {
 };
 
 const options = new chrome.Options();
-const date = new Date();
 options.addArguments("--user-data-dir=./profile");
+const newOpts = new chrome.Options();
+newOpts.addArguments("--user-data-dir=/tmp");
 
 var currDriver = false;
 
-setInterval(getInfo,1000);
+var cutoffDriver = new webdriver.Builder()
+.forBrowser('chrome')
+.withCapabilities(webdriver.Capabilities.chrome())
+.setChromeOptions(options)
+.build();
+
+var cutoffDriver2 = new webdriver.Builder()
+.forBrowser('chrome')
+.withCapabilities(webdriver.Capabilities.chrome())
+.setChromeOptions(newOpts)
+.build();
+
+//setInterval(getCutoffInfo,100);
 
 function getCutoffElements(driver){
     return new Promise(function(resolve){
-        driver.get("http://game.granbluefantasy.jp/#event/teamraid041/ranking").then(()=>{
+        driver.get("http://game.granbluefantasy.jp/#event/teamraid042/ranking").then(()=>{
             driver.wait(Until.elementsLocated(By.className('txt-total-record')),10000,"no ele").then(function(){
                 driver.findElements(By.className('txt-total-record')).then(elements=>{
                     var first = elements[9].getText();
@@ -38,48 +51,45 @@ function getCutoffElements(driver){
                     var fourth = elements[15].getText();
                     Promise.all([first,second,third,fourth]).then(vals=>{resolve(vals)});
                 });
-            });
+            }).catch(()=>{console.log("waiting for login")});
         }); 
     });
 }
 
-function saveToJson(cutOffData,callback = function(){}){
-    fs.readFile("./cutoffs.json",'utf8',function(err,data){
+function saveToJson(GWdata,date,file,callback = function(){}){
+    fs.readFile(file,'utf8',function(err,data){
         if(err){
             throw err;
         }else{
             var obj = JSON.parse(data);
             var dateStr = ""+monthInfo[date.getMonth()]+" " + date.getDate()+ ", "+date.getHours()+":"+date.getMinutes();
-            var cutOffMap = {
-                "30k" : cutOffData[0],
-                "50k" : cutOffData[1],
-                "70k" : cutOffData[2],
-                "120k" : cutOffData[3]
+            var map = {
+                "30k" : GWdata[0],
+                "50k" : GWdata[1],
+                "70k" : GWdata[2],
+                "120k" : GWdata[3]
             };
-            var cutOffInfo = {};
-            cutOffInfo[dateStr] = cutOffMap;
-            obj.push(cutOffInfo);
+            var info = {};
+            info[dateStr] = map;
+            obj.push(info);
             var json = JSON.stringify(obj,null,4);
-            fs.writeFile('cutoffs.json',json,callback);
+            fs.writeFile(file,json,callback);
         }
     });
 }
 
-function getInfo(){
+function getCutoffInfo(){
+    var date = new Date();
     if(date.getMinutes()%10 == 0 && !currDriver){
         currDriver = true;
-        let driver = new webdriver.Builder()
-        .forBrowser('chrome')
-        .withCapabilities(webdriver.Capabilities.chrome())
-        .setChromeOptions(options)
-        .build();
-
-        getCutoffElements(driver).then(eles=>{
-            saveToJson(eles,function(){
+        getCutoffElements(cutoffDriver).then(eles=>{
+            saveToJson(eles,date,'./json/cutoffs.json',function(){
                 console.log("saved to cutoffs.json");
                 console.log("saved at: ",""+monthInfo[date.getMonth()]+" " + date.getDate()+ ", "+date.getHours()+":"+date.getMinutes());
-                driver.quit();
             });
         });
+    }
+    if(date.getMinutes()%10 == 1 && currDriver){
+        currDriver = false;
     }
 }
